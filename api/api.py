49322230ba17a986ea9bffbb756c5720cd7479d3
Mahan <mahan.abbuspour@gmail.com>
2020-03-27 02:35:15 +0430

    Create API to interact with the celery worker

---
+++ api/api.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+from url_crawler import crawler
+from config import config
+
+app = Flask(__name__)
+
+
+@app.route("/crawler", methods=["POST"])
+def crawler_endpoint():
+    if request.method == "POST":
+        data = request.form
+        url = data['url']
+        # check the url against a regex
+        if url:
+            crawler.url_crawler.delay(url)
+            return 'Running\n'
+        else:
+            return 'Bad UEL:{}'.format(url)
+
+
+app.run()
